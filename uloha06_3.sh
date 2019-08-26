d1c34647272985850afae92caf7d38f672012d35
Radka Kváčová <cygne@kali.domacesko>
2019-08-26 13:15:58 +0200

    ukol

--- uloha06_3.sh
+++ uloha06_3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#prvni sloupec je pocet lidi a druhy jmeno skupiny
-skupiny="$( cat $1 | awk 'BEGIN { FS=":" } { print($3, $1) }' )"
+skupiny="$( cat $1 | sed 's/\([a-zA-Z]\+\):x:[0-9]\+:/\1:/' | awk 'BEGIN { FS="," } { print(NF,$1) }' )"
+
+skupiny="$( echo "$skupiny" | sed 's/\([a-zA-Z]\+\):[a-zA-Z]*/\1/' )"
 
 #setridi podle poctu lidi ve skupine, od nejvetsiho cisla
 skupiny="$( echo "$skupiny" | sort -gr)"
