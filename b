448486312e3abbec962ff15e5dfdfd1019989777
Radka Kváčová <cygne@kali.domacesko>
2019-08-26 13:46:02 +0200

    ukol

---
+++ b
@@ -0,0 +1,3 @@
+cygne:x:42:a,b,c
+cygnee:x:2:c,d,e
+cygneee:x:999:cy,ox
--- uloha06_3.sh
+++ uloha06_3.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-skupiny="$( cat $1 | sed 's/\([a-zA-Z]\+\):x:[0-9]\+:/\1:/' | awk 'BEGIN { FS="," } { print(NF,$1) }' )"
-
-skupiny="$( echo "$skupiny" | sed 's/\([a-zA-Z]\+\):[a-zA-Z]*/\1/' )"
+#nazev skupiny ma splnovat [_a-z][-0-9_a-z]*\$?
+skupiny="$( cat $1 | sed 's/\([_a-z][-0-9_a-z]*\$?\):x:[0-9]\+:[a-zA-Z]\+,/\1,/' | awk 'BEGIN { FS="," } { print(NF,$1) }' )"
 
 #setridi podle poctu lidi ve skupine, od nejvetsiho cisla
 skupiny="$( echo "$skupiny" | sort -gr)"
